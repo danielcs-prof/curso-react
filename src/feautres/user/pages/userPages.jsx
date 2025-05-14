0c3baec26ff82babca0ffe31b74d9224c22226d1
Daniel C. Silva <danielcs80@gmail.com>
2025-05-14 18:48:58 -0300

    UserPage atualizada

---
+++ src/feautres/user/pages/userPages.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import UserForm from "../components/userForm";
+import UserTable from "../components/userTables";
+
+export default function UserPage(){
+    const [users, setUsers] = useState([]);
+      const [editIndex, setEditIndex] = useState(null);
+    
+      const handleAddOrUpdateUser = (user) => {
+        if (editIndex === null) {
+          setUsers([...users, user]);
+        } else {
+          const updatedUsers = [...users];
+          updatedUsers[editIndex] = user;
+          setUsers(updatedUsers);
+          setEditIndex(null);
+        }
+      };
+    
+      const handleEditUser = (index) => {
+        setEditIndex(index);
+      };
+    
+      const handleDeleteUser = (index) => {
+        const updatedUsers = users.filter((_, i) => i !== index);
+        setUsers(updatedUsers);
+        if (editIndex === index) {
+          setEditIndex(null);
+        }
+      };
+    return (
+        <div className="container mt-4">
+              <h2 className="mb-4">Gerenciar Usuários</h2>
+              <UserForm
+                onSubmit={handleAddOrUpdateUser}
+                editUser={editIndex !== null ? users[editIndex] : null}
+              />
+              <UserTable users={users} onEdit={handleEditUser} onDelete={handleDeleteUser} />
+            </div>
+    );
+}
